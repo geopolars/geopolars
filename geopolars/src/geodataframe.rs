337e66b06a49837b187294f12688108ce0df5d04
Stuart Lynn <stuart.lynn@gmail.com>
2022-06-09 23:53:17 -0400

    Adds convex_hull to geoseries and geodataframe (#18)
    * Adds convex_hull to geoseries and geodataframe
    
    * Moving convex_hull to be in alphabetical order.

--- geopolars/src/geodataframe.rs
+++ geopolars/src/geodataframe.rs
@@ -3,11 +3,17 @@ use polars::prelude::{DataFrame, Result, Series};
 
 pub trait GeoDataFrame {
     fn centroid(&self) -> Result<Series>;
+    fn convex_hull(&self) -> Result<Series>;
 }
 
 impl GeoDataFrame for DataFrame {
     fn centroid(&self) -> Result<Series> {
         let geom_column = self.column("geometry")?;
         geom_column.centroid()
+    }
+
+    fn convex_hull(&self) -> Result<Series> {
+        let geom_column = self.column("geometry")?;
+        geom_column.convex_hull()
     }
 }
--- py-geopolars/src/lib.rs
+++ py-geopolars/src/lib.rs
@@ -13,8 +13,18 @@ fn centroid(series: &PyAny) -> PyResult<PyObject> {
     ffi::rust_series_to_py_series(&out)
 }
 
+#[pyfunction]
+fn convex_hull(series: &PyAny) -> PyResult<PyObject> {
+    let series = ffi::py_series_to_rust_series(series)?;
+    let out = series
+        .convex_hull()
+        .map_err(|e| PyValueError::new_err(format!("Something went wrong: {:?}", e)))?;
+    ffi::rust_series_to_py_series(&out)
+}
+
 #[pymodule]
 fn geopolars(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(centroid)).unwrap();
+    m.add_wrapped(wrap_pyfunction!(convex_hull)).unwrap();
     Ok(())
 }
