680484f277d403a33f4f5e093d1d601697b25de8
Stuart Lynn <stuart.lynn@gmail.com>
2022-06-13 11:19:02 -0500

    Fixing up python bindings

--- py-geopolars/src/lib.rs
+++ py-geopolars/src/lib.rs
@@ -1,6 +1,6 @@
 mod ffi;
 
-use geopolars::geoseries::GeoSeries;
+use geopolars::geoseries::{GeoSeries, GeodesicLengthMethod};
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 
@@ -23,28 +23,19 @@ fn convex_hull(series: &PyAny) -> PyResult<PyObject> {
 }
 
 #[pyfunction]
-fn euclidean_distance(series: &PyAny) -> PyResult<PyObject> {
+fn euclidean_length(series: &PyAny) -> PyResult<PyObject> {
     let series = ffi::py_series_to_rust_series(series)?;
     let out = series
-        .euclidean_distance()
+        .euclidean_length()
         .map_err(|e| PyValueError::new_err(format!("Something went wrong: {:?}", e)))?;
     ffi::rust_series_to_py_series(&out)
 }
 
 #[pyfunction]
-fn geodesic_distance(series: &PyAny) -> PyResult<PyObject> {
+fn geodesic_length(series: &PyAny) -> PyResult<PyObject> {
     let series = ffi::py_series_to_rust_series(series)?;
     let out = series
-        .geodesic_distance()
-        .map_err(|e| PyValueError::new_err(format!("Something went wrong: {:?}", e)))?;
-    ffi::rust_series_to_py_series(&out)
-}
-
-#[pyfunction]
-fn haversine_distance(series: &PyAny) -> PyResult<PyObject> {
-    let series = ffi::py_series_to_rust_series(series)?;
-    let out = series
-        .haversine_distance()
+        .geodesic_length(GeodesicLengthMethod::Geodesic)
         .map_err(|e| PyValueError::new_err(format!("Something went wrong: {:?}", e)))?;
     ffi::rust_series_to_py_series(&out)
 }
@@ -53,8 +44,7 @@ fn haversine_distance(series: &PyAny) -> PyResult<PyObject> {
 fn geopolars(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(centroid)).unwrap();
     m.add_wrapped(wrap_pyfunction!(convex_hull)).unwrap();
-    m.add_wrapped(wrap_pyfunction!(euclidean_distance).unwrap());
-    m.add_wrapped(wrap_pyfunction!(geodesic_distance).unwrap());
-    m.add_wrapped(wrap_pyfunction!(haversine_distance).unwrap());
+    m.add_wrapped(wrap_pyfunction!(euclidean_length)).unwrap();
+    m.add_wrapped(wrap_pyfunction!(geodesic_length)).unwrap();
     Ok(())
 }
