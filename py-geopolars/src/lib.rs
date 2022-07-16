d27cd0d14bc19049659201aabe3b977a23365201
Kyle Barron <kylebarron2@gmail.com>
2022-07-16 12:13:08 -0600

    Improved python error handling

--- py-geopolars/src/lib.rs
+++ py-geopolars/src/lib.rs
@@ -1,5 +1,7 @@
+mod error;
 mod ffi;
 
+use crate::error::PyGeopolarsError;
 use geopolars::geoseries::{GeoSeries, GeodesicLengthMethod};
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
@@ -7,27 +9,21 @@ use pyo3::prelude::*;
 #[pyfunction]
 fn centroid(series: &PyAny) -> PyResult<PyObject> {
     let series = ffi::py_series_to_rust_series(series)?;
-    let out = series
-        .centroid()
-        .map_err(|e| PyValueError::new_err(format!("Something went wrong: {:?}", e)))?;
+    let out = series.centroid().map_err(PyGeopolarsError::from)?;
     ffi::rust_series_to_py_geoseries(&out)
 }
 
 #[pyfunction]
 fn convex_hull(series: &PyAny) -> PyResult<PyObject> {
     let series = ffi::py_series_to_rust_series(series)?;
-    let out = series
-        .convex_hull()
-        .map_err(|e| PyValueError::new_err(format!("Something went wrong: {:?}", e)))?;
+    let out = series.convex_hull().map_err(PyGeopolarsError::from)?;
     ffi::rust_series_to_py_geoseries(&out)
 }
 
 #[pyfunction]
 fn euclidean_length(series: &PyAny) -> PyResult<PyObject> {
     let series = ffi::py_series_to_rust_series(series)?;
-    let out = series
-        .euclidean_length()
-        .map_err(|e| PyValueError::new_err(format!("Something went wrong: {:?}", e)))?;
+    let out = series.euclidean_length().map_err(PyGeopolarsError::from)?;
     ffi::rust_series_to_py_series(&out)
 }
 
@@ -46,25 +42,23 @@ fn geodesic_length(series: &PyAny, method: &str) -> PyResult<PyObject> {
 
     let out = series
         .geodesic_length(rust_method)
-        .map_err(|e| PyValueError::new_err(format!("Something went wrong: {:?}", e)))?;
+        .map_err(PyGeopolarsError::from)?;
     ffi::rust_series_to_py_series(&out)
 }
 
 #[pyfunction]
 fn to_crs(series: &PyAny, from: &str, to: &str) -> PyResult<PyObject> {
     let series = ffi::py_series_to_rust_series(series)?;
-    let out = series
-        .to_crs(from, to)
-        .map_err(|e| PyValueError::new_err(format!("Something went wrong: {:?}", e)))?;
+    let out = series.to_crs(from, to).map_err(PyGeopolarsError::from)?;
     ffi::rust_series_to_py_series(&out)
 }
 
 #[pymodule]
 fn geopolars(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(centroid)).unwrap();
-    m.add_wrapped(wrap_pyfunction!(convex_hull)).unwrap();
-    m.add_wrapped(wrap_pyfunction!(euclidean_length)).unwrap();
-    m.add_wrapped(wrap_pyfunction!(geodesic_length)).unwrap();
-    m.add_wrapped(wrap_pyfunction!(to_crs)).unwrap();
+    m.add_wrapped(wrap_pyfunction!(centroid))?;
+    m.add_wrapped(wrap_pyfunction!(convex_hull))?;
+    m.add_wrapped(wrap_pyfunction!(euclidean_length))?;
+    m.add_wrapped(wrap_pyfunction!(geodesic_length))?;
+    m.add_wrapped(wrap_pyfunction!(to_crs))?;
     Ok(())
 }
