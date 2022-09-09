0de2a4cbccd402a9c412878aad51f459f8ba85a5
Kyle Barron <kylebarron2@gmail.com>
2022-09-09 22:07:30 +0200

    Lint

--- py-geopolars/python/geopolars/internals/geodataframe.py
+++ py-geopolars/python/geopolars/internals/geodataframe.py
@@ -54,10 +54,10 @@ class GeoDataFrame(pl.DataFrame):
     def geometry(self):
         return GeoSeries(self.get_column(self._geometry_column_name))
 
-
     @classmethod
     def from_geopandas(cls, geodataframe):
         from geopandas.io.arrow import _geopandas_to_arrow
+
         arrow_table = _geopandas_to_arrow(geodataframe)
         polars_df = polars.from_arrow(arrow_table)
         return cls(polars_df)
@@ -71,11 +71,13 @@ class GeoDataFrame(pl.DataFrame):
                     "encoding": "WKB",
                     "geometry_type": "Unknown",
                     "crs": None,
-                    "edges": "planar"
+                    "edges": "planar",
                 }
             },
             "primary_column": self._geometry_column_name,
-            "version": "0.4.0"
+            "version": "0.4.0",
         }
         arrow_table = self.to_arrow()
-        return _arrow_to_geopandas(arrow_table, metadata={b'geo': json.dumps(geoarrow_metadata).encode()})
+        return _arrow_to_geopandas(
+            arrow_table, metadata={b"geo": json.dumps(geoarrow_metadata).encode()}
+        )
