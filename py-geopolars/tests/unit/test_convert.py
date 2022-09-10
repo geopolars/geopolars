a75358bd0f22944b54196a6051b4d71f6bbb93e0
Kyle Barron <kylebarron2@gmail.com>
2022-09-10 23:36:26 +0200

    Add to geopandas

--- py-geopolars/tests/unit/test_convert.py
+++ py-geopolars/tests/unit/test_convert.py
@@ -19,3 +19,26 @@ class TestFromGeoPandas:
         )
         geoseries = gpl.from_geopandas(geopandas_gdf.geometry)
         assert geoseries == ne_cities_gdf.get_column("geometry")
+
+
+class TestToGeoPandas:
+    def test_gdf_to_geopandas(self, ne_cities_gdf: gpl.GeoDataFrame):
+        geopandas_gdf = ne_cities_gdf.to_geopandas()
+        assert isinstance(geopandas_gdf, geopandas.GeoDataFrame)
+
+    def test_geoseries_to_geopandas(self, ne_cities_gdf: gpl.GeoDataFrame):
+        geopandas_geoseries = ne_cities_gdf.get_column("geometry").to_geopandas()
+        assert isinstance(geopandas_geoseries, geopandas.GeoSeries)
+
+
+class TestRoundTripGeoPandas:
+    def test_gdf_round_trip(self, ne_cities_gdf: gpl.GeoDataFrame):
+        geopandas_gdf = ne_cities_gdf.to_geopandas()
+        new_gdf = gpl.GeoDataFrame._from_geopandas(geopandas_gdf)
+        assert new_gdf == ne_cities_gdf
+
+    def test_geoseries_round_trip(self, ne_cities_gdf: gpl.GeoDataFrame):
+        geoseries = ne_cities_gdf.geometry
+        geopandas_geoseries = geoseries.to_geopandas()
+        new_geoseries = gpl.GeoSeries._from_geopandas(geopandas_geoseries)
+        assert new_geoseries == geoseries
