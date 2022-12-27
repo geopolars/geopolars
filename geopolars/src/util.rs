700f52542c1f6b1c28dea3250134b17026778f2d
Kyle Barron <kylebarron2@gmail.com>
2022-12-27 11:34:27 -0800

    series index to chunks index

--- geopolars/src/util.rs
+++ geopolars/src/util.rs
@@ -2,6 +2,7 @@ use geo::{Coord, Geometry, LineString, Point, Polygon};
 use geozero::{wkb::Wkb, ToGeo};
 use polars::datatypes::{AnyValue, DataType};
 use polars::export::arrow::array::{Array, ListArray, PrimitiveArray, StructArray};
+use polars::export::num;
 use polars::prelude::{PolarsError, PolarsResult, Series};
 
 /// Helper function to iterate over geometries from polars Series
@@ -175,4 +176,42 @@ fn get_chunk_and_local_index(series: &Series, global_index: usize) -> (Box<dyn A
 pub enum Predicate {
     Intersects,
     Contains,
+}
+
+/// Get the index of the chunk and the index of the value in that chunk
+// From: https://github.com/pola-rs/polars/blob/f8bb5aaa9bb8f8c3c9365933a062758478fb63ad/polars/polars-core/src/chunked_array/ops/downcast.rs#L76-L83
+#[inline]
+pub(crate) fn index_to_chunked_index(series: &Series, index: usize) -> (usize, usize) {
+    if series.chunks().len() == 1 {
+        return (0, index);
+    }
+
+    _index_to_chunked_index(series.chunk_lengths(), index)
+}
+
+/// This logic is same as the impl on ChunkedArray
+/// The difference is that there is less indirection because the caller should preallocate
+/// `chunk_lens` once. On the `ChunkedArray` we indirect through an `ArrayRef` which is an indirection
+/// and a vtable.
+// From: https://github.com/pola-rs/polars/blob/f8bb5aaa9bb8f8c3c9365933a062758478fb63ad/polars/polars-core/src/utils/mod.rs#L822-L846
+#[inline]
+pub(crate) fn _index_to_chunked_index<
+    I: Iterator<Item = Idx>,
+    Idx: PartialOrd + std::ops::AddAssign + std::ops::SubAssign + num::Zero + num::One,
+>(
+    chunk_lens: I,
+    index: Idx,
+) -> (Idx, Idx) {
+    let mut index_remainder = index;
+    let mut current_chunk_idx = num::Zero::zero();
+
+    for chunk_len in chunk_lens {
+        if chunk_len > index_remainder {
+            break;
+        } else {
+            index_remainder -= chunk_len;
+            current_chunk_idx += num::One::one();
+        }
+    }
+    (current_chunk_idx, index_remainder)
 }
