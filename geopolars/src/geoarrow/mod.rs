41beacf12b7d2282984394ea55d91af820253071
Kyle Barron <kylebarron2@gmail.com>
2022-12-27 15:46:11 -0800

    Mutable polygon array

--- geopolars/src/geoarrow/mod.rs
+++ geopolars/src/geoarrow/mod.rs
@@ -1,4 +1,3 @@
 pub mod linestring;
 pub mod point;
 pub mod polygon;
-pub mod util;
--- geopolars/src/geoarrow/polygon/mutable.rs
+++ geopolars/src/geoarrow/polygon/mutable.rs
@@ -5,24 +5,30 @@ use polars::export::arrow::offset::OffsetsBuffer;
 use polars::prelude::ArrowField;
 
 #[derive(Debug, Clone)]
-pub struct MutableLineStringArray {
+pub struct MutablePolygonArray {
     x: Vec<f64>,
     y: Vec<f64>,
-    offsets: Vec<i64>,
+    ring_offsets: Vec<i64>,
+    geom_offsets: Vec<i64>,
 
     /// Validity is only defined at the geometry level
     validity: Option<MutableBitmap>,
 }
 
-impl MutableLineStringArray {
+impl MutablePolygonArray {
     pub fn into_arrow(self) -> ListArray<i64> {
         // Data type
         let coord_field_x = ArrowField::new("x", DataType::Float64, false);
         let coord_field_y = ArrowField::new("y", DataType::Float64, false);
         let struct_data_type = DataType::Struct(vec![coord_field_x, coord_field_y]);
-        let list_data_type = DataType::LargeList(Box::new(ArrowField::new(
+        let inner_list_data_type = DataType::LargeList(Box::new(ArrowField::new(
             "vertices",
             struct_data_type,
+            false,
+        )));
+        let outer_list_data_type = DataType::LargeList(Box::new(ArrowField::new(
+            "rings",
+            inner_list_data_type,
             false,
         )));
 
@@ -32,6 +38,10 @@ impl MutableLineStringArray {
         } else {
             None
         };
+
+        // Offsets
+        let ring_offsets_buffer = unsafe { OffsetsBuffer::new_unchecked(self.ring_offsets.into()) };
+        let geom_offsets_buffer = unsafe { OffsetsBuffer::new_unchecked(self.geom_offsets.into()) };
 
         // Array data
         let array_x = Box::new(PrimitiveArray::<f64>::from_values(self.x)) as Box<dyn Array>;
@@ -43,9 +53,18 @@ impl MutableLineStringArray {
             None,
         )) as Box<dyn Array>;
 
-        // Offsets
-        let offsets_buffer = unsafe { OffsetsBuffer::new_unchecked(self.offsets.into()) };
+        let inner_list_array = Box::new(ListArray::new(
+            inner_list_data_type,
+            ring_offsets_buffer,
+            coord_array,
+            None,
+        )) as Box<dyn Array>;
 
-        ListArray::new(list_data_type, offsets_buffer, coord_array, validity)
+        ListArray::new(
+            outer_list_data_type,
+            geom_offsets_buffer,
+            inner_list_array,
+            validity,
+        )
     }
 }
