3fe80777622c10066fd1dc9ad15ba5ab8da840c4
Kyle Barron <kylebarron2@gmail.com>
2023-01-11 20:52:24 -0500

    update cargo lockfile

--- geopolars/src/lib.rs
+++ geopolars/src/lib.rs
@@ -1,10 +1,14 @@
 pub mod error;
-pub mod geoarrow;
 pub mod geodataframe;
 pub mod geoseries;
 pub mod ops;
 pub mod spatial_index;
 pub mod util;
+
+pub use geopolars_arrow;
+pub use geopolars_geo;
+#[cfg(feature = "geos")]
+pub use geopolars_geos;
 
 #[cfg(test)]
 mod tests {
--- geopolars/src/util.rs
+++ geopolars/src/util.rs
@@ -10,27 +10,6 @@ use polars::export::num;
 use polars::prelude::{PolarsError, PolarsResult, Series};
 use std::convert::Into;
 
-pub enum GeoArrowType {
-    Point,
-    LineString,
-    Polygon,
-    WKB,
-}
-
-pub fn get_geoarrow_type(series: &Series) -> GeoArrowType {
-    match series.dtype() {
-        DataType::Binary => GeoArrowType::WKB,
-        DataType::Struct(_) => GeoArrowType::Point,
-        DataType::List(dt) => match *dt.clone() {
-            DataType::Struct(_) => GeoArrowType::LineString,
-            DataType::List(_) => GeoArrowType::Polygon,
-            _ => panic!("Unexpected inner list type: {}", dt),
-        },
-
-        dt => panic!("Unexpected geoarrow type: {}", dt),
-    }
-}
-
 pub fn from_geom_vec(geoms: &[Geometry<f64>]) -> Result<Series> {
     let mut wkb_array = MutableBinaryArray::<i32>::with_capacity(geoms.len());
 
@@ -202,42 +181,4 @@ fn geom_at_index_polygon(series: &Series, index: usize) -> PolarsResult<Geometry
 pub enum Predicate {
     Intersects,
     Contains,
-}
-
-/// Get the index of the chunk and the index of the value in that chunk
-// From: https://github.com/pola-rs/polars/blob/f8bb5aaa9bb8f8c3c9365933a062758478fb63ad/polars/polars-core/src/chunked_array/ops/downcast.rs#L76-L83
-#[inline]
-pub(crate) fn index_to_chunked_index(series: &Series, index: usize) -> (usize, usize) {
-    if series.chunks().len() == 1 {
-        return (0, index);
-    }
-
-    _index_to_chunked_index(series.chunk_lengths(), index)
-}
-
-/// This logic is same as the impl on ChunkedArray
-/// The difference is that there is less indirection because the caller should preallocate
-/// `chunk_lens` once. On the `ChunkedArray` we indirect through an `ArrayRef` which is an indirection
-/// and a vtable.
-// From: https://github.com/pola-rs/polars/blob/f8bb5aaa9bb8f8c3c9365933a062758478fb63ad/polars/polars-core/src/utils/mod.rs#L822-L846
-#[inline]
-pub(crate) fn _index_to_chunked_index<
-    I: Iterator<Item = Idx>,
-    Idx: PartialOrd + std::ops::AddAssign + std::ops::SubAssign + num::Zero + num::One,
->(
-    chunk_lens: I,
-    index: Idx,
-) -> (Idx, Idx) {
-    let mut index_remainder = index;
-    let mut current_chunk_idx = num::Zero::zero();
-
-    for chunk_len in chunk_lens {
-        if chunk_len > index_remainder {
-            break;
-        } else {
-            index_remainder -= chunk_len;
-            current_chunk_idx += num::One::one();
-        }
-    }
-    (current_chunk_idx, index_remainder)
 }
