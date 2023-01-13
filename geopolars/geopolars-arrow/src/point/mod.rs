10b6ee71ca5ea678ca04e9bdaf2e756de676d596
Kyle Barron <kylebarron2@gmail.com>
2023-01-13 16:28:53 -0500

    Implement PointArray

--- geopolars/geopolars-arrow/src/point/mod.rs
+++ geopolars/geopolars-arrow/src/point/mod.rs
@@ -1,7 +1,8 @@
 //! Helpers for using Point GeoArrow data
 
-pub use array::{PointArray, PointArrayParts, PointSeries};
+pub use array_old::{PointArray, PointArrayParts, PointSeries};
 pub use mutable::MutablePointArray;
 
-mod array;
+mod array_old;
 mod mutable;
+mod array;
