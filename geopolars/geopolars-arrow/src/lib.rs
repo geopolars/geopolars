f6faeba9df765ea31bebf389def3334c100d0c80
Kyle Barron <kylebarron2@gmail.com>
2023-01-16 01:09:28 -0500

    update top level geoarrow exports (#197)

--- geopolars/geopolars-arrow/src/lib.rs
+++ geopolars/geopolars-arrow/src/lib.rs
@@ -1,16 +1,16 @@
 //! Helpers for working with GeoArrow geometries
 //!
-//! At some point in the future, this will likely become a public geoarrow module, or be integrated
-//! into geozero
+//! At some point in the future, this will likely become a public standalone geoarrow module, or be
+//! integrated into geozero
 
 pub use binary::{MutableWKBArray, WKBArray};
 pub use enum_::{GeometryArrayEnum, GeometryType};
-pub use linestring::LineStringArray;
-pub use multilinestring::MultiLineStringArray;
-pub use multipoint::MultiPointArray;
-pub use multipolygon::MultiPolygonArray;
+pub use linestring::{LineStringArray, MutableLineStringArray};
+pub use multilinestring::{MultiLineStringArray, MutableMultiLineStringArray};
+pub use multipoint::{MultiPointArray, MutableMultiPointArray};
+pub use multipolygon::{MultiPolygonArray, MutableMultiPolygonArray};
 pub use point::{MutablePointArray, PointArray};
-pub use polygon::PolygonArray;
+pub use polygon::{MutablePolygonArray, PolygonArray};
 pub use trait_::GeometryArray;
 
 pub mod binary;
