675ee00c2a24ae17ae99e03dba0311ff0230e9fd
Kyle Barron <kylebarron2@gmail.com>
2023-01-16 19:56:16 -0500

    isolate geopolars-geo tests from polars (#198)

--- geopolars/geopolars-geo/src/ops/convex_hull.rs
+++ geopolars/geopolars-geo/src/ops/convex_hull.rs
@@ -67,17 +67,12 @@ pub(crate) fn convex_hull(array: GeometryArrayEnum) -> Result<GeometryArrayEnum>
 
 #[cfg(test)]
 mod tests {
-    use crate::geoseries::GeoSeries;
-    use geo::{line_string, polygon, MultiPoint, Point};
-    use geopolars_arrow::polygon::PolygonArray;
-    use geopolars_arrow::{LineStringArray, MultiPointArray};
-    use polars::prelude::Series;
+    use super::convex_hull;
+    use geo::{line_string, polygon, Geometry, MultiPoint, Point};
+    use geopolars_arrow::{GeometryArrayEnum, LineStringArray, MultiPointArray};
 
     #[test]
     fn convex_hull_for_multipoint() {
-        // NOTE: this actually gets interpreted as a LineString not MultiPoint due to inferring
-        // type from arrow schema when parsing from a series
-
         // Values borrowed from this test in geo crate: https://docs.rs/geo/0.14.2/src/geo/algorithm/convexhull.rs.html#323
         let input_geom: MultiPoint = vec![
             Point::new(0.0, 10.0),
@@ -92,11 +87,7 @@ mod tests {
         ]
         .into();
         let input_array: MultiPointArray = vec![input_geom].into();
-        let input_series =
-            Series::try_from(("geometry", input_array.into_arrow().boxed())).unwrap();
-
-        let result_series = input_series.convex_hull().unwrap();
-        let result_array: PolygonArray = result_series.chunks()[0].clone().try_into().unwrap();
+        let result_array = convex_hull(GeometryArrayEnum::MultiPoint(input_array)).unwrap();
 
         let expected = polygon![
             (x:0.0, y: -10.0),
@@ -106,7 +97,10 @@ mod tests {
             (x:0.0, y:-10.0),
         ];
 
-        assert_eq!(expected, result_array.get_as_geo(0).unwrap());
+        assert_eq!(
+            Geometry::Polygon(expected),
+            result_array.get_as_geo(0).unwrap()
+        );
     }
 
     #[test]
@@ -122,12 +116,9 @@ mod tests {
             (x: -1.0, y: 1.0),
             (x: 0.0, y: 10.0),
         ];
-        let input_array: LineStringArray = vec![input_geom].into();
-        let input_series =
-            Series::try_from(("geometry", input_array.into_arrow().boxed())).unwrap();
 
-        let result_series = input_series.convex_hull().unwrap();
-        let result_array: PolygonArray = result_series.chunks()[0].clone().try_into().unwrap();
+        let input_array: LineStringArray = vec![input_geom].into();
+        let result_array = convex_hull(GeometryArrayEnum::LineString(input_array)).unwrap();
 
         let expected = polygon![
             (x: 0.0, y: -10.0),
@@ -137,6 +128,9 @@ mod tests {
             (x: 0.0, y: -10.0),
         ];
 
-        assert_eq!(expected, result_array.get_as_geo(0).unwrap());
+        assert_eq!(
+            Geometry::Polygon(expected),
+            result_array.get_as_geo(0).unwrap()
+        );
     }
 }
