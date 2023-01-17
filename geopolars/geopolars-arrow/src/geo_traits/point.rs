431c95a51c205e115fdbef40d3f6adacef1dba3c
Kyle Barron <kylebarron2@gmail.com>
2023-01-17 01:37:45 -0500

    add test

--- geopolars/geopolars-arrow/src/geo_traits/point.rs
+++ geopolars/geopolars-arrow/src/geo_traits/point.rs
@@ -38,3 +38,27 @@ impl PointTrait for Coord<f64> {
         (self.x, self.y)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::PointTrait;
+    use crate::PointArray;
+
+    #[test]
+    fn test_point_function_geo() {
+        fn identity(point: &impl PointTrait) -> &impl PointTrait {
+            point
+        }
+
+        let point = geo::point!(x: 1., y: 2.);
+        let output = identity(&point);
+
+        assert_eq!(point.x_y(), output.x_y());
+
+        let arrow_point_array: PointArray = vec![point].into();
+        let arrow_point_scalar = &arrow_point_array.get(0).unwrap();
+        let output_arrow_point_scalar = identity(arrow_point_scalar);
+
+        assert_eq!(arrow_point_scalar.x(), output_arrow_point_scalar.x());
+    }
+}
