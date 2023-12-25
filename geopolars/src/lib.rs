e5ae62f3b6997355abd2ff45b7972213aaad04ad
Kyle Barron <kylebarron2@gmail.com>
2023-12-25 09:54:41 -0800

    cleanup

--- geopolars/src/lib.rs
+++ geopolars/src/lib.rs
@@ -1,19 +1,3 @@
-pub mod ops;
-pub mod spatial_index;
-pub mod util;
+// mod spatial_index;
 
-pub use geoarrow;
-pub use geopolars_geo;
-#[cfg(feature = "geos")]
-pub use geopolars_geos;
-
-pub use geopolars_geo::error;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+pub use geopolars_geo::*;
